c257dd0543289392e8e2cbc0e4885377e177a15a
Blacksuan19 <abubakaryagob@gmail.com>
2022-08-16 20:29:06 +0800

    redash_python: add build and publish action
    Signed-off-by: Blacksuan19 <abubakaryagob@gmail.com>

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-requirements = [
-    "requests",
-]
-setup(
-    name="redash-python",
-    version="0.0.2",
-    author="Abubakar Yagoub",
-    author_email="i@blacksuan19.dev",
-    description="A more complete Python client for the Redash API",
-    packages=find_packages(),
-    requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
