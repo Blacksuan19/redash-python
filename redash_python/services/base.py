95340581f97b0656260d15d7c681031929260262
Blacksuan19 <abubakaryagob@gmail.com>
2022-08-19 14:42:09 +0800

    redash_python: use Dict for all operations
    simplenamespace adds unnecessary complexity
    
    - fixes duplicate dashboards
    - fixes create_query_table
    
    Signed-off-by: Blacksuan19 <abubakaryagob@gmail.com>

--- redash_python/services/base.py
+++ redash_python/services/base.py
@@ -1,9 +1,6 @@
-from types import SimpleNamespace
-from typing import Optional, final
+from typing import Dict, Optional, final
 
 import requests
-
-from ..utils.json_encoder import ModelEncoder
 
 
 class BaseService:
@@ -23,35 +20,31 @@ class BaseService:
         self,
         method: str,
         endpoint: str,
-        data: Optional[SimpleNamespace] = None,
+        data: Optional[Dict] = None,
         **kwargs,
-    ) -> SimpleNamespace:
+    ) -> Dict:
         """Perform a request to the endpoint with the given data"""
         url = self.base_url + endpoint
-
-        if not isinstance(data, dict):
-            data = self.__encoder.encode(data)
-
         response = self.__session.request(
-            method, url, json=data if data else None, **kwargs
+            method,
+            url,
+            json=data if data else None,
+            **kwargs,
         )
-
         response.raise_for_status()
-        return response.json(object_hook=lambda d: SimpleNamespace(**d))
+        return response.json()
 
     @final
-    def post(self, endpoint: str, data: SimpleNamespace, **kwargs) -> SimpleNamespace:
+    def post(self, endpoint: str, data: Dict, **kwargs) -> Dict:
         """Perform a POST request to the endpoint with the given data"""
         return self._request("POST", endpoint, data, **kwargs)
 
     @final
-    def get(
-        self, endpoint: str, data: Optional[SimpleNamespace] = None, **kwargs
-    ) -> SimpleNamespace:
+    def get(self, endpoint: str, data: Optional[Dict] = None, **kwargs) -> Dict:
         """Perform a GET request to the endpoint"""
         return self._request("GET", endpoint, data, **kwargs)
 
     @final
-    def delete(self, endpoint: str, **kwargs) -> SimpleNamespace:
+    def delete(self, endpoint: str, **kwargs) -> Dict:
         """Perform a DELETE request to the endpoint"""
         return self._request("DELETE", endpoint, **kwargs)
