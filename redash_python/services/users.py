5f153802bb62d243b4010e16494be77297db1131
Blacksuan19 <abubakaryagob@gmail.com>
2023-06-07 13:41:03 +0800

    services: users: implement pagination mixin
    - fixes get_all and exists methods
    
    Signed-off-by: Blacksuan19 <abubakaryagob@gmail.com>

--- redash_python/services/users.py
+++ redash_python/services/users.py
@@ -1,13 +1,19 @@
+from typing import Dict, List
+
 from .base import BaseService
-from .mixins import CommonMixin, NameMixin, PrintMixin
+from .mixins import CommonMixin, NameMixin, PaginationMixin, PrintMixin
 
 
-class UsersService(CommonMixin, NameMixin, PrintMixin):
+class UsersService(CommonMixin, NameMixin, PrintMixin, PaginationMixin):
     def __init__(self, base: BaseService) -> None:
         CommonMixin.__init__(self, base)
+        PaginationMixin.__init__(self, base)
 
         self.__base = base
         self.endpoint = "/api/users"
+
+    def get_all(self) -> List[Dict]:
+        return self.paginate()
 
     def disable(self, user_id: int) -> None:
         """
